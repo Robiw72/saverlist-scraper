fa153c0636638f176b0bbc57626cb200d22473fb
robiw72 <robiw72@gmail.com>
2026-07-13 22:51:10 +0200

    Fix: config.py portabile (legge da env vars), rimossa da gitignore

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+API_URL = os.environ.get(
+    "API_URL",
+    "https://robiw72.altervista.org/SaverList_Market/api.php"
+)
+
+PARSE_BOT_API_KEY = os.environ.get("PARSE_BOT_API_KEY", "")
